27a5ca3aa932719b4a3f4b2f2b3e49214219e291
System User <manjmin6@gmail.com>
2024-03-25 17:27:07 +0900

    timm Block with BitLinear

--- bitnet_block.py
+++ bitnet_block.py
@@ -1,8 +1,68 @@
-from timm.layers import Mlp, DropPath
+from timm.layers import Mlp, DropPath, use_fused_attn
 from timm.models.vision_transformer import Block
 import torch
 import torch.nn as nn
 from torch.nn.modules import GELU, LayerNorm, Module
+from torch.jit import Final
+from bitnet import BitLinear, BitFeedForward
+import torch.nn.functional as F
+
+
+class Attention(nn.Module):
+    fused_attn: Final[bool]
+
+    def __init__(
+        self,
+        dim: int,
+        num_heads: int = 8,
+        qkv_bias: bool = False,
+        qk_norm: bool = False,
+        attn_drop: float = 0.0,
+        proj_drop: float = 0.0,
+        norm_layer: nn.Module = nn.LayerNorm,
+    ) -> None:
+        super().__init__()
+        assert dim % num_heads == 0, "dim should be divisible by num_heads"
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+        self.scale = self.head_dim**-0.5
+        self.fused_attn = use_fused_attn()
+
+        self.qkv = BitLinear(dim, dim * 3, bias=qkv_bias)
+        self.q_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
+        self.k_norm = norm_layer(self.head_dim) if qk_norm else nn.Identity()
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = BitLinear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B, N, C = x.shape
+        qkv = (
+            self.qkv(x)
+            .reshape(B, N, 3, self.num_heads, self.head_dim)
+            .permute(2, 0, 3, 1, 4)
+        )
+        q, k, v = qkv.unbind(0)
+        q, k = self.q_norm(q), self.k_norm(k)
+
+        if self.fused_attn:
+            x = F.scaled_dot_product_attention(
+                q,
+                k,
+                v,
+                dropout_p=self.attn_drop.p if self.training else 0.0,
+            )
+        else:
+            q = q * self.scale
+            attn = q @ k.transpose(-2, -1)
+            attn = attn.softmax(dim=-1)
+            attn = self.attn_drop(attn)
+            x = attn @ v
+
+        x = x.transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
 
 
 class LayerScale(nn.Module):
@@ -20,7 +80,7 @@ class LayerScale(nn.Module):
         return x.mul_(self.gamma) if self.inplace else x * self.gamma
 
 
-class bitBlock(Block):
+class bitBlock(nn.Module):
     def __init__(
         self,
         dim: int,
@@ -36,19 +96,16 @@ class bitBlock(Block):
         norm_layer: Module = nn.LayerNorm,
         mlp_layer: Module = ...,
     ) -> None:
-        super().__init__(
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(
             dim,
-            num_heads,
-            mlp_ratio,
-            qkv_bias,
-            qk_norm,
-            proj_drop,
-            attn_drop,
-            init_values,
-            drop_path,
-            act_layer,
-            norm_layer,
-            mlp_layer,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_norm=qk_norm,
+            attn_drop=attn_drop,
+            proj_drop=proj_drop,
+            norm_layer=norm_layer,
         )
         self.ls1 = (
             LayerScale(dim, init_values=init_values) if init_values else nn.Identity()
@@ -56,12 +113,14 @@ class bitBlock(Block):
         self.drop_path1 = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
 
         self.norm2 = norm_layer(dim)
-        self.mlp = mlp_layer(
-            in_features=dim,
-            hidden_features=int(dim * mlp_ratio),
-            act_layer=act_layer,
-            drop=proj_drop,
+
+        self.bitmlp = BitFeedForward(
+            dim=dim,
+            mult=mlp_ratio,
+            glu=bool(act_layer),
+            dropout=proj_drop,
         )
+
         self.ls2 = (
             LayerScale(dim, init_values=init_values) if init_values else nn.Identity()
         )
@@ -69,5 +128,5 @@ class bitBlock(Block):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = x + self.drop_path1(self.ls1(self.attn(self.norm1(x))))
-        x = x + self.drop_path2(self.ls2(self.mlp(self.norm2(x))))
+        x = x + self.drop_path2(self.ls2(self.bitmlp(self.norm2(x))))
         return x
